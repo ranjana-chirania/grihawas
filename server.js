e653c4e652e9c117477c700949b34cb2a873f875
Ranjana Chirania <work.ranjanachirania@email.com>
2026-09-21 00:15:08 +0530

    Update server port for Render

--- server.js
+++ server.js
@@ -221,7 +221,7 @@ app.post("/api/apply", async (req, res) => {
 
 createAdmin();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
